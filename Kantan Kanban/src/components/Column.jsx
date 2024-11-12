730f0f9265d108dbcf57524a4073df9143994d2d
Adam Measom <adameasom@mail.com>
2024-11-13 06:16:21 +0900

    Improve legibility on buttons

--- Kantan Kanban/src/components/Column.jsx
+++ Kantan Kanban/src/components/Column.jsx
@@ -22,7 +22,13 @@ function Column({ status, tasks, addTask, deleteTask }) {
             }
           }}
         >
-          <button className="add-button" type="submit" disabled={!taskTitle} data-tooltip-id="my-add" data-tooltip-content="Add Task" data-tooltip-place="top">
+          <button
+            className="add-button"
+            type="submit"
+            disabled={!taskTitle}
+            data-tooltip-id="my-add"
+            data-tooltip-content="Add Task"
+            data-tooltip-place="top">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" strokeWidth={1} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
