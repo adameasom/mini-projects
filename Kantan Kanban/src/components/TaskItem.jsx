b86feb02845a061196309f6404fe708c7fd54880
Adam Measom <adameasom@mail.com>
2024-11-15 09:00:57 +0900

    Add missing brackets for onClick

--- Kantan Kanban/src/components/TaskItem.jsx
+++ Kantan Kanban/src/components/TaskItem.jsx
@@ -60,8 +60,8 @@ function TaskItem({ task, deleteTask, updateTask, status, handleVibration }) {
           />
           <div className="edit-buttons-box">
             <div className="edit-save-or-cancel">
-              <button className="edit-save my-text-button" onClick={() => { handleSaveEditClick; handleVibration(50); }}>Save</button>
-              <button className="edit-cancel my-text-button" onClick={() => { handleCancelEditClick; handleVibration(20); }}>Cancel</button>
+              <button className="edit-save my-text-button" onClick={() => { handleSaveEditClick(); handleVibration(50); }}>Save</button>
+              <button className="edit-cancel my-text-button" onClick={() => { handleCancelEditClick(); handleVibration(20); }}>Cancel</button>
             </div>
             <button className="fake-button my-button">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" strokeWidth={1} stroke="currentColor" className="size-6">
@@ -92,7 +92,7 @@ function TaskItem({ task, deleteTask, updateTask, status, handleVibration }) {
             <span className={`date-created ${isConfirmVisible ? 'fade' : ''}`}>created at {task.dateCreated}</span>
             <button
               className="delete-button my-button"
-              onClick={() => { handleDeleteClick; handleVibration(20); }}
+              onClick={() => { handleDeleteClick(); handleVibration(20); }}
               data-tooltip-id={`my-delete-${task.id}`}
               data-tooltip-content="Delete Task"
               data-tooltip-place="top-start"
@@ -103,8 +103,8 @@ function TaskItem({ task, deleteTask, updateTask, status, handleVibration }) {
             </button>
             <Tooltip id={`my-delete-${task.id}`} className="custom-tooltip" />
             <div className={`confirmation-box ${isConfirmVisible ? 'show' : ''}`}>
-              <button className="confirm-delete my-text-button" onClick={() => { handleConfirmDelete; handleVibration(150); }}>Delete</button>
-              <button className="confirm-cancel my-text-button" onClick={() => { handleCancel; handleVibration(20); }}>Cancel</button>
+              <button className="confirm-delete my-text-button" onClick={() => { handleConfirmDelete(); handleVibration(150); }}>Delete</button>
+              <button className="confirm-cancel my-text-button" onClick={() => { handleCancel(); handleVibration(20); }}>Cancel</button>
             </div>
           </div>
         </div>
